2fb832a89df1c645187a3dd9e44f4545f000838f
Jamayne <jamaynedanquah@gmail.com>
2022-03-19 13:52:23 +0000

    First commit

---
+++ src/components/CreateItem.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { useMoralis, useMoralisQuery } from "react-moralis";
+import { useMoralisDapp } from "providers/MoralisDappProvider/MoralisDappProvider";
+import { Table, Tag, Space } from "antd";
+import { PolygonCurrency} from "./Chains/Logos";
+import moment from "moment";
+
+const styles = {
+  table: {
+    margin: "0 auto",
+    width: "1000px",
+  },
+};
+
+function CreateItem() {
+    
+};
+
+export default CreateItem;
